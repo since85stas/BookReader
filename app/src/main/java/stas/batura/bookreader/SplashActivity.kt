0a37835c96a62153891b571ff00168bb150643be
Note <seeyou85@gmail.com>
2021-05-02 23:48:27 +0300

    добавлено приветствие

---
+++ app/src/main/java/stas/batura/bookreader/SplashActivity.kt
@@ -0,0 +1,30 @@
+package stas.batura.bookreader
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hiding title bar of this activity
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        //making this activity full screen
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+        //4second splash time
+        Handler().postDelayed({
+            //start main activity
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            //finish this activity
+            finish()
+        },4000)
+
+    }
+
+}
