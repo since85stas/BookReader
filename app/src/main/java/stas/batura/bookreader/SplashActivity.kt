07c0990a6da74a2f630f79c132f55176491f5a71
Note <seeyou85@gmail.com>
2021-05-03 21:17:16 +0300

    добавлена анимация в SplashActivity.kt

--- app/src/main/java/stas/batura/bookreader/SplashActivity.kt
+++ app/src/main/java/stas/batura/bookreader/SplashActivity.kt
@@ -5,9 +5,16 @@ import android.os.Bundle
 import android.os.Handler
 import android.view.Window
 import android.view.WindowManager
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_splash.*
+
 
 class SplashActivity: AppCompatActivity() {
+
+    private var mFadeInAnimation: Animation? = null
+    private  var mFadeOutAnimation:Animation? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,8 +30,41 @@ class SplashActivity: AppCompatActivity() {
             startActivity(Intent(this@SplashActivity, MainActivity::class.java))
             //finish this activity
             finish()
-        },4000)
+        }, 2000)
 
+        // подключаем файл анимации
+        mFadeInAnimation = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        mFadeOutAnimation = AnimationUtils.loadAnimation(this, R.anim.fadeout);
+        mFadeInAnimation!!.setAnimationListener(animationFadeInListener);
+        mFadeOutAnimation!!.setAnimationListener(animationFadeOutListener);
+
+        // при запуске начинаем с анимации исчезновения
+        welcText.startAnimation(mFadeOutAnimation);
+
+    }
+
+    var animationFadeOutListener: Animation.AnimationListener = object : Animation.AnimationListener {
+        override fun onAnimationEnd(animation: Animation) {
+            welcText.startAnimation(mFadeInAnimation)
+        }
+
+        override fun onAnimationRepeat(animation: Animation) {
+        }
+
+        override fun onAnimationStart(animation: Animation) {
+        }
+    }
+
+    var animationFadeInListener: Animation.AnimationListener = object : Animation.AnimationListener {
+        override fun onAnimationEnd(animation: Animation) {
+            welcText.startAnimation(mFadeOutAnimation)
+        }
+
+        override fun onAnimationRepeat(animation: Animation) {
+        }
+
+        override fun onAnimationStart(animation: Animation) {
+        }
     }
 
 }
